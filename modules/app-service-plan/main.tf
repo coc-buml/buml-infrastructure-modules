8165a74ce8de92a4979ced263cd23b31ef5bab40
Philipp Schmid <32632186+philschmid@users.noreply.github.com>
2020-10-26 15:38:55 +0100

    Update main.tf

--- modules/app-service-plan/main.tf
+++ modules/app-service-plan/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_app_service_plan" "service_plan" {
   name                = var.service_plan_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  kind                = "Linux"
+  kind                = "elastic"
   tags                = var.tags
   reserved            = true
   sku {
