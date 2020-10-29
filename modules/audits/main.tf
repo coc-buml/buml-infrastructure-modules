e06c51f246ec97f2d95b13261fcde40df912466f
Philipp Schmid <schmidphilipp1995@gmail.com>
2020-10-29 09:49:50 +0100

    changed var

--- modules/audits/main.tf
+++ modules/audits/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_monitor_diagnostic_setting" "core" {
   name               = var.diagnostic_name
-  target_resource_id = azurerm_virtual_network.vnet.id
+  target_resource_id = var.target_resource_id
   log_analytics_workspace_id = var.log_analytics_workspace_id
   log = var.log
   metric = var.metric
