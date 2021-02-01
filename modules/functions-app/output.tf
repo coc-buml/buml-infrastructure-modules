1028b8557438b79d952f5c1b0ce9ad23f1898f85
Philipp Schmid <32632186+philschmid@users.noreply.github.com>
2021-02-01 11:34:15 +0100

    Update output.tf

--- modules/functions-app/output.tf
+++ modules/functions-app/output.tf
@@ -7,3 +7,8 @@ output "function_name" {
   description = "The name of the Function App. Changing this forces a new resource to be created."
   value = azurerm_function_app.core.name
 }
+
+output "function_id" {
+  description = "The id of the Function App."
+  value = azurerm_function_app.core.id
+}
