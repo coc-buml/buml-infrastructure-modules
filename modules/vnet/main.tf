3e64a799fdfdb23ce288c87038a1219cd1056192
Maximilian Hänsel <mh@maxhaensel.de>
2021-04-07 09:24:16 +0200

    [BUML-434] ddos for vnet

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,12 +1,20 @@
 
-
 resource "azurerm_virtual_network" "vnet" {
+  depends_on = [azurerm_network_ddos_protection_plan.buml_ddos_protection]
   name                = var.vnet_name
   resource_group_name = var.resource_group_name
   location            = var.location
   address_space       = [var.address_space]
   dns_servers         = var.dns_servers
   tags                = var.tags
+
+  /*
+    Secure the Azure VNET behind a ddos plan
+  */
+  ddos_protection_plan {
+    id     = azurerm_network_ddos_protection_plan.buml_ddos_protection.id
+    enable = true
+  }
 }
 
 resource "azurerm_subnet" "subnet" {
@@ -17,13 +25,23 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   service_endpoints    = var.service_endpoints
   delegation {
-      name = "delegation"
-      service_delegation {
-          actions = [
-                "Microsoft.Network/virtualNetworks/subnets/action",
-            ]
-          name    = "Microsoft.Web/serverFarms"
-        }
+    name = "delegation"
+    service_delegation {
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/action",
+      ]
+      name = "Microsoft.Web/serverFarms"
     }
   }
+}
 
+/*
+  Link to Ressoruces: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_ddos_protection_plan
+  We need a DDoS-Protection to fulfill the regulations
+*/
+resource "azurerm_network_ddos_protection_plan" "buml_ddos_protection" {
+  name                = "buml-ddos-protection-plan-${var.location}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = var.location
+  tags                = var.tags
+}
